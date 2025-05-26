91eabb8f260706a0ea23f692cce8a135fa23a4da
Janiece Thomas <bookmissj@gmail.com>
2025-05-26 01:13:15 -0400

    chore: remove unused React imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+// src/App.tsx
+
 import ServiceCatalog from './components/ServiceCatalog'
+import BookingForm    from './components/BookingForm'
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <ServiceCatalog />
+      {/* or <BookingForm /> */}
     </div>
   )
 }
